1735ea7ae4e630dd4a5d3a440f713631501a7a80
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-09-18 13:56:22 +0300

    Define types, then API calls

---
+++ src/services/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import type { Post, NewPost  } from "../types/Post";
+
+// Store the base URL in a variable
+const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
+
+// Fetch all required posts from the BASE_URL
+export async function fetchPosts(limit:10) : Promise<Post[]> {
+    const res = await axios.get<Post[]>(`${BASE_URL}?_limit=${limit}`)
+    return res.data
+}
+
+// Create a post and add it to the available posts
+export async function createPost(newPost: NewPost) : Promise<Post> {
+    const res = await axios.post<Post>(BASE_URL, newPost)
+    return res.data
+}
+
+// Edit a post and update the posts
+export async function updatePost(id: number, updated: Post) : Promise<Post> {
+    const res = await axios.put<Post>(`${BASE_URL}/${id}`, updated)
+return res.data
+}
+
+// Delete a post and remove from other posts
+export async function daletePost(id: number) : Promise<void>{
+    await axios.delete(`${BASE_URL}/${id}`)
+}
---
+++ src/types/Post.ts
@@ -0,0 +1,11 @@
+// Define Types
+export interface Post{
+    id: number; //
+    title: string
+    body: string
+}
+
+export interface NewPost{
+    title: string
+    body: string
+}
